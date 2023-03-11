4a48609ec4a997360cf69929f1dc9c26151ddc2d
LorenzoDv <lorenzo.devichi@hotmail.fr>
2023-03-11 11:53:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+
+$( document ).ready(function() {
+    $("video").prop("volume", 0.2);
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        console.log( scroll );
+        if(scroll >=537){
+            $('#topAptitude').addClass('hr')
+            $('#botAptitude').addClass('hr1')
+
+            $('.apt-center p')
+        }
+        else if(scroll <=100){
+            $('#topAptitude').removeClass('hr')
+            $('#botAptitude').removeClass('hr1')
+
+
+        }
+    });
+
+    $( "figure" ).each(function( index ) {
+        $( this ).click(function() {
+            let myImg = $(this).find("img");
+            let myDesc = $(this).find(".desc");
+            let myVideo = $(this).find("video");
+            myImg.hide()
+            myDesc.hide()
+            myVideo.get(0).play()
+        })
+        $( this ).mouseleave(function() {
+            let myImg = $(this).find("img");
+            let myDesc = $(this).find(".desc");
+            let myVideo = $(this).find("video");
+            myImg.show()
+            myDesc.show()
+            myVideo.get(0).pause()
+        })
+
+    });
+});
